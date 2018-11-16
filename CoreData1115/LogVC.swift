876896b9f793271537b508ef7406887840cae383
jiyeshin <krjiyeah@outlook.kr>
2018-11-16 10:57:47 +0900

    iOSDB

---
+++ CoreData1115/LogVC.swift
@@ -0,0 +1,109 @@
+//
+//  LogVC.swift
+//  CoreData1115
+//
+//  Created by 503-12 on 16/11/2018.
+//  Copyright © 2018 The. All rights reserved.
+//
+
+import UIKit
+
+//열거형 상수
+//자료형을 LogType으로 설정한 것임.
+public enum LogType: Int16{
+    case create = 0
+    case edit = 1
+}
+
+//클래스의 기능을 확장하기 위한 extension
+//LogType이라는 클래스에는 toLogType이라는 메소드가 추가되는것과 같음.
+extension Int16{
+    func toLogType() -> String{
+        switch self{
+        case 0: return "생성"
+        case 1: return "수정"
+        default: return ""
+        }
+    }
+}
+
+class LogVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HR1114/ViewController.swift
+++ HR1114/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  iOSDB
+//  HR1114
 //
 //  Created by 503-12 on 14/11/2018.
 //  Copyright © 2018 The. All rights reserved.
@@ -8,10 +8,14 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
 
-    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
 
 }
